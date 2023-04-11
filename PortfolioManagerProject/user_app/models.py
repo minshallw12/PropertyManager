1dd678f463567b33f000d2afe17ef96f9485a99c
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-04-11 14:06:28 -0600

    figuring out git

--- PortfolioManagerProject/user_app/models.py
+++ PortfolioManagerProject/user_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class App_User(AbstractUser):
+    email = models.EmailField(blank=False, null=False, unique=True, max_length=254)
+    name = models.CharField(null = False, blank= False, max_length=50)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.name} | {self.email}"
