c4445bda2ff6cc122c0ae9488fdabf9afe2624f7
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-04-11 14:00:53 -0600

    PROJECT-003: First save point

--- PortfolioManagerProject/user_app/urls.py
+++ PortfolioManagerProject/user_app/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.send_the_index, name="index"),
-
-
+    path('', views.send_the_index, name='index')
 ]
--- PortfolioManagerProject/user_app/views.py
+++ PortfolioManagerProject/user_app/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def sign_in(request):
-    return HttpResponse('This is the sign in page.')
 
 def send_the_index(request):
     the_index = open('static/index.html')
-    return HttpResponse(the_index) 
+    return HttpResponse(the_index)
