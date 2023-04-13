f5f1ed62ac2f87857618d2e923f81048852c4f1e
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-04-13 15:56:09 -0600

    PROJECT-004: Prepared for first API integration.

--- PortfolioManagerProject/frontend/src/components/NavBar.jsx
+++ PortfolioManagerProject/frontend/src/components/NavBar.jsx
@@ -15,7 +15,7 @@ export const NavBar = () => {
                             <Link className="nav_links" to={'/dashboard'}>My Properties</Link> 
                             <Link className="nav_links" to={'/dashboard'}>My Managers</Link>
                             <Link className="nav_links" to={'/dashboard'}>Search</Link> 
-                            <Link className="nav_links" to={'/dashboard'}>Calculator</Link> 
+                            <Link className="nav_links" to={'/dashboard/calc'}>Calculator</Link> 
                         </div>
                     :
                         <div className="navbar">
---
+++ PortfolioManagerProject/frontend/src/pages/Calculator.jsx
@@ -0,0 +1,8 @@
+export default function Calculator() {
+
+    return (
+        <div className="calculator_page center">
+            <h3>API form here</h3>
+        </div>
+    )
+}
