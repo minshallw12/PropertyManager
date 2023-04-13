f8011a2cf10634176f26eb600614eb0b63b8c010
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-04-13 16:09:08 -0600

    stopping point

--- PortfolioManagerProject/frontend/src/pages/Calculator.jsx
+++ PortfolioManagerProject/frontend/src/pages/Calculator.jsx
@@ -2,7 +2,12 @@ export default function Calculator() {
 
     return (
         <div className="calculator_page center">
-            <h3>API form here</h3>
+            <div className="header">
+                <div className="center">
+                    <h1>Amortization Calculator</h1>
+                </div>
+                <p>Amortization is paying off a debt over time in equal installments. Part of each payment goes toward the loan principal, and part goes toward interest. As the loan amortizes, the amount going toward principal starts out small, and gradually grows larger month by month. In an amortization schedule, you can see how much money you pay in principal and interest over time. Use this calculator to input the details of your loan and see how those payments break down over your loan term.</p>
+            </div>
         </div>
     )
 }
