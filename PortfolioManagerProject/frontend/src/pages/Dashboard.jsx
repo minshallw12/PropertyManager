d65c4ea0fb657c24584efa6cbe804887a638d5c3
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-04-18 15:34:45 -0600

    PROJECT-005: Added dashboard metrics to homepage.

--- PortfolioManagerProject/frontend/src/pages/Dashboard.jsx
+++ PortfolioManagerProject/frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,37 @@
 import Map from "../components/Map"
+import inventory from '../data/my_props.json'
+
+
+export function totalSF(lst) {
+        let sum = 0
+        for (let i=0; i<lst.length; i++) {
+            sum += lst[i]['square_feet']
+        }
+        return sum
+    };
+
+export function totalIncome(lst) {
+    let sum = 0
+    for (let i=0; i<lst.length; i++) {
+        sum += lst[i]['current_income']
+    }
+    return sum
+};
+
+export function totalUpkeep(lst) {
+
+    let sum= 0
+    for (let i=0; i<lst.length; i++) {
+        sum += lst[i]['current_upkeep']
+    }
+    return sum
+};
 
 export default function Dashboard() {
+    const totalSquareFeet = totalSF(inventory);
+    const totalMonthlyIncome = totalIncome(inventory);
+    const totalMonthlyUpkeep = totalUpkeep(inventory);
+    const totalMonthlyProfit = totalMonthlyIncome - totalMonthlyUpkeep;
 
 
     return (
@@ -10,17 +41,20 @@ export default function Dashboard() {
             <div className="dashboard">
                 <div className="metrics_container">
 
+                    <div><h4>Total Units</h4></div>
+                    <div className="center"><h2>{inventory.length}</h2></div>
+                    
                     <div><h4>Total square feet</h4></div>
-                    <div className="center"><h2>SQFT</h2></div>
+                    <div className="center"><h2>{totalSquareFeet} sqft</h2></div>
 
                     <div><h4>Total monthly income</h4></div>
-                    <div className="center"><h2>$ INCOME</h2></div>
+                    <div className="center"><h2>$ {totalMonthlyIncome}</h2></div>
 
                     <div><h4>Total monthly costs</h4></div>
-                    <div className="center"><h2>$ COSTS</h2></div>
+                    <div className="center"><h2>$ {totalMonthlyUpkeep}</h2></div>
 
                     <div><h4>Total monthly profit</h4></div>
-                    <div className="center"><h2>$ PROFIT</h2></div>
+                    <div className="center"><h2>$ {totalMonthlyProfit}</h2></div>
                     
                 </div>
 
