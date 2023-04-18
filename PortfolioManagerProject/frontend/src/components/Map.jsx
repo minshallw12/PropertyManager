10955e943f26f1fc5156344ce5ca6471c37d4281
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-04-18 14:13:53 -0600

    PROJECT-005: Getting coordinates response, still need to thread coordinates into marker.

--- PortfolioManagerProject/frontend/src/components/Map.jsx
+++ PortfolioManagerProject/frontend/src/components/Map.jsx
@@ -1,7 +1,8 @@
-import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api"
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import Geocode from 'react-geocode';
 
 const center = { lat:48.8584, lng: 2.2945 }
-
+const address = "Louvre"
 
 export default function Map() {
 
@@ -14,6 +15,27 @@ export default function Map() {
         return <h3>Loading...</h3>// loading screen
     }
 
+    Geocode.setApiKey('AIzaSyCxg5D2bcU4yPiwsFF0204BwCGkCLcDR1k')
+    Geocode.setLanguage('en');
+    Geocode.setRegion('us');
+
+    let coords;
+    async function addressToCoordinates(my_address) {
+        let response = await 
+        Geocode.fromAddress(my_address).then(
+            (response) => {
+                return response.results[0].geometry.location;
+            },
+            (error) => {
+                console.error(error);
+            }
+        )
+        console.log(response, "response")
+        return response
+    }
+
+    coords = addressToCoordinates(address)
+    console.log(coords, 'coords')
 
     return (
         <div className="google_map">
