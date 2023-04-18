251208dcee4a2ada37428f1f9e5ee90c9aafafcd
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-04-17 18:12:02 -0600

    PROJECT-OO5: Prepped to add second API

--- PortfolioManagerProject/frontend/src/pages/Dashboard.jsx
+++ PortfolioManagerProject/frontend/src/pages/Dashboard.jsx
@@ -1,3 +1,46 @@
 export default function Dashboard() {
-    return <h1>You made it to the dashboard</h1>
+
+
+    
+    return (
+        <div className="dashboard_container">
+            <div className="center">
+            </div>
+            <div className="dashboard">
+                <div className="metrics_container">
+                    <div>
+                        <h4>Total square feet</h4>
+                    </div>
+                    <div className="center">
+                        <h2>SQFT</h2>
+                    </div>
+                    <div>
+                        <h4>Total monthly income</h4>
+                    </div>
+                    <div className="center">
+                        <h2>$ INCOME</h2>
+                    </div>
+                    <div>
+                        <h4>Total monthly costs</h4>
+                    </div>
+                    <div className="center">
+                        <h2>$ COSTS</h2>
+                    </div>
+                    <div>
+                        <h4>Total monthly profit</h4>
+                    </div>
+                    <div className="center">
+                        <h2>$ PROFIT</h2>
+                    </div>
+                </div>
+
+                <div className="map_container">
+                    <img src="" alt="google map here" />
+                </div>
+            </div>
+
+            
+        </div>
+        
+    )
 }
